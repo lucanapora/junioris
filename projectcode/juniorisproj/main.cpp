b16a6a594955ce9da310cc68719dac7826e0740e
lucanapora <lucanapora@gmail.com>
2023-02-27 21:23:39 -0500

    nothing working

--- projectcode/juniorisproj/main.cpp
+++ projectcode/juniorisproj/main.cpp
@@ -1,51 +1,35 @@
-//
-//  main.cpp
-//  juniorisproj
-//
-//  Created by Luca Napora on 2/21/23.
-//
-//#ifdef __APPLE__
-//#include <OpenGL/gl3.h>
-//#include <OpenGL/gl3ext.h>
-//#endif
-//allows dynamic version of library
-#include <GL/glew.h>
-#define GLFW_DLL
-#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+#include <stdio.h>
 
-int main(void)
+
+static void RenderSceneCB()
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Luca Napora", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
 }
 
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    int width = 1920;
+    int height = 1080;
+    glutInitWindowSize(width, height);
+
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Hello World);
+    printf("window id: %d\n", win);
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}

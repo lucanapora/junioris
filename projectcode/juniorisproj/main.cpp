8fc36a65fa8c303362c0c81908aeeb39c095af6f
lucanapora <lucanapora@gmail.com>
2023-02-27 20:19:21 -0500

    black window with glew

--- projectcode/juniorisproj/main.cpp
+++ projectcode/juniorisproj/main.cpp
@@ -4,21 +4,14 @@
 //
 //  Created by Luca Napora on 2/21/23.
 //
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef __APPLE__
-#include <OpenGL/gl3.h>
-#include <OpenGL/gl3ext.h>
-#endif
-
-//#include <GL/glew.h>
+//#ifdef __APPLE__
+//#include <OpenGL/gl3.h>
+//#include <OpenGL/gl3ext.h>
+//#endif
+//allows dynamic version of library
+#include <GL/glew.h>
+#define GLFW_DLL
 #include <GLFW/glfw3.h>
-//include shader file
-#include "shader.hpp"
-
-//using namespace glm;
-
 
 int main(void)
 {
@@ -32,49 +25,12 @@ int main(void)
     window = glfwCreateWindow(640, 480, "Luca Napora", NULL, NULL);
     if (!window)
     {
-        fprintf( stderr, "Failed to open GLFW window\n");
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
-    
-    //trying to make a triangle
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-    
-    //three points initialized to make a triangle
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
-    };
-    
-    //create a buffer so the triangle can be given to OpenGL
-    GLuint vertexbuffer;
-    //creates one buffer who's identifier is stored in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    //give our vertices to OpenGL
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    
-    glDrawArrays(GL_TRIANGLES,0,3); //this is where you start
-    glDisableVertexAttribArray(0);
-        
-                          
-                          
-    GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
-    
-    glClear(GL_COLOR_BUFFER_BIT);
-    glUseProgram(programID);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -92,3 +48,4 @@ int main(void)
     glfwTerminate();
     return 0;
 }
+
